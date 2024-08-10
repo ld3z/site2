382a6234a63b9ea99a42e12d91fc852d921dcd40
ld3z <ldeziangamer@gmail.com>
2024-08-10 11:19:49 -0700

    remove empty columns?

--- scripts/pf-table.py
+++ scripts/pf-table.py
@@ -2,4 +2,5 @@ import pandas as pd
 
 def on_pre_build(config, **kwargs) -> None:
     df = pd.read_csv('https://docs.google.com/spreadsheets/d/1dlWL4NQ0J0wrUJyrlmRMmmG9msarkuZtXOoI5ADLyP4/gviz/tq?tqx=out:csv&sheet=spezi&gid=1214029714&headers=1')
+    df.drop("", 1)
     df.to_csv("docs/pf_data.csv")
