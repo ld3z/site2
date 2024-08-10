a1abce2e843e5212905b4b52542020c32007e7e6
ld3z <ldeziangamer@gmail.com>
2024-08-10 10:32:13 -0700

    start changes and hopefully add table?

--- docs/phantom-forces.md
+++ docs/phantom-forces.md
@@ -3,3 +3,5 @@
 This is not so much a guide as it is a resource page.
 
 # **WIP**
+
+This is a table: {{ read_csv("docs/pf_data.csv") }}
---
+++ scripts/pf-table.py
@@ -0,0 +1,5 @@
+import pandas as pd
+
+def on_pre_build(config, **kwargs) -> None:
+    df = pd.read_csv('https://docs.google.com/spreadsheets/d/1dlWL4NQ0J0wrUJyrlmRMmmG9msarkuZtXOoI5ADLyP4/gviz/tq?tqx=out:csv&sheet=spezi&gid=1214029714')
+    df.to_csv("docs/pf_data.csv")
