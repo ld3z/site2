2a5e6e58fd8268932d2812668b482a607dd92ae7
ld3zian <42660576+ld3z@users.noreply.github.com>
2024-08-10 13:17:01 -0700

    fix?

--- scripts/pf-table.py
+++ scripts/pf-table.py
@@ -3,5 +3,5 @@ import pandas as pd
 def on_pre_build(config, **kwargs) -> None:
     df = pd.read_csv('https://docs.google.com/spreadsheets/d/1dlWL4NQ0J0wrUJyrlmRMmmG9msarkuZtXOoI5ADLyP4/gviz/tq?tqx=out:csv&sheet=spezi&gid=1214029714&headers=1')
     df.dropna(axis="columns", how="all", inplace=True)
-    df.drop([0], axis=1)
+    df.drop([1], axis=1)
     df.to_csv("docs/pf_data.csv")
