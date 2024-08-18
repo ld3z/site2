105653503e212dcdd9d1b47913fda3cacb1c8cf1
ld3zian <42660576+ld3z@users.noreply.github.com>
2024-08-17 18:02:27 -0700

    test?

--- scripts/pf-table.py
+++ scripts/pf-table.py
@@ -1,6 +1,9 @@
 import pandas as pd
 
 def on_pre_build(config, **kwargs) -> None:
-    df = pd.read_csv('https://docs.google.com/spreadsheets/d/1dlWL4NQ0J0wrUJyrlmRMmmG9msarkuZtXOoI5ADLyP4/gviz/tq?tqx=out:csv&sheet=spezi&gid=1214029714&headers=0&range=D8:K33')
-    df.dropna(axis="columns", how="all", inplace=True)
-    df.to_csv("docs/pf_assault_rifles.csv", index=False)
+    df1 = pd.read_csv('https://docs.google.com/spreadsheets/d/1dlWL4NQ0J0wrUJyrlmRMmmG9msarkuZtXOoI5ADLyP4/gviz/tq?tqx=out:csv&sheet=spezi&gid=1214029714&headers=0&range=D8:K33')
+    df2 = pd.read_csv('https://docs.google.com/spreadsheets/d/1dlWL4NQ0J0wrUJyrlmRMmmG9msarkuZtXOoI5ADLyP4/gviz/tq?tqx=out:csv&sheet=spezi&gid=1214029714&headers=0&range=A8:B33')
+    df1.dropna(axis="columns", how="all", inplace=True)
+    df2.dropna(axis="columns", how="all", inplace=True)
+    df1.join(df2)
+    df1.to_csv("docs/pf_assault_rifles.csv", index=False)
