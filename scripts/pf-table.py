efb59fc25793f09ef7f72859160bf58362b84930
ld3zian <42660576+ld3z@users.noreply.github.com>
2024-08-17 17:10:06 -0700

    hmmm

--- scripts/pf-table.py
+++ scripts/pf-table.py
@@ -2,5 +2,4 @@ import pandas as pd
 
 def on_pre_build(config, **kwargs) -> None:
     df = pd.read_csv('https://docs.google.com/spreadsheets/d/1dlWL4NQ0J0wrUJyrlmRMmmG9msarkuZtXOoI5ADLyP4/gviz/tq?tqx=out:html&sheet=spezi&gid=1214029714&headers=1&range=D8:K33')
-    df.dropna(axis="columns", how="all", inplace=True)
     df.to_csv("docs/pf_assault_rifles.csv", index=False)
