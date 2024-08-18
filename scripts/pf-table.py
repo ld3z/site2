74ae323a5870c7314b5bb5fe0441a64c69426732
ld3zian <42660576+ld3z@users.noreply.github.com>
2024-08-17 18:05:58 -0700

    idk

--- scripts/pf-table.py
+++ scripts/pf-table.py
@@ -5,5 +5,5 @@ def on_pre_build(config, **kwargs) -> None:
     df2 = pd.read_csv('https://docs.google.com/spreadsheets/d/1dlWL4NQ0J0wrUJyrlmRMmmG9msarkuZtXOoI5ADLyP4/gviz/tq?tqx=out:csv&sheet=spezi&gid=1214029714&headers=0&range=A8:B33')
     df1.dropna(axis="columns", how="all", inplace=True)
     df2.dropna(axis="columns", how="all", inplace=True)
-    df1.join(df2)
+    df1.merge(df2, how="left")
     df1.to_csv("docs/pf_assault_rifles.csv", index=False)
