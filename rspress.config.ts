c1a86ee5a754e3719a3769e45ad264e804ecc90a
ld3zian <42660576+ld3z@users.noreply.github.com>
2025-08-15 16:08:21 -0700

    tailwind

--- rspress.config.ts
+++ rspress.config.ts
@@ -3,6 +3,7 @@ import { defineConfig } from "rspress/config";
 
 export default defineConfig({
   root: path.join(__dirname, "docs"),
+  globalStyles: path.join(__dirname, 'styles/index.css'),
   title: "ldez's Site",
   icon: "/favicon.png",
   base: "/site2/",
