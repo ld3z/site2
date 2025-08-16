5aef370516a9e74eaceeeb0f8b2ce586d5e2ae42
ld3zian <42660576+ld3z@users.noreply.github.com>
2025-08-15 17:11:35 -0700

    oops

--- rspress.config.ts
+++ rspress.config.ts
@@ -3,7 +3,6 @@ import { defineConfig } from "rspress/config";
 
 export default defineConfig({
   root: path.join(__dirname, "docs"),
-  globalStyles: path.join(__dirname, 'styles/index.css'),
   title: "ldez's Site",
   icon: "/favicon.png",
   base: "/site2/",
